9a84fd247421b59afc7ab3bf7b6d9fb5a99b3058
Hubert Lewandowski <therootender@gmail.com>
2023-09-25 20:28:44 +0200

    Refactor file

--- robot/camera/src/main.cpp
+++ robot/camera/src/main.cpp
@@ -2,17 +2,22 @@
 
 int main(int argc, char *argv[])
 {
-    VideoStream stream("Test stream");
+    auto const streamName   = "Test stream";
+    auto const serverIp     = "127.0.0.1";
+    auto const serverPort   = 8080;
+
+    auto pipeline = "v4l2src device=/dev/video0 ! videoconvert ! autovideosink";
+
+    VideoStream stream(streamName);
 
     try 
     {
-        // This is an example pipeline.
-        // Modify it to your liking
-        stream.SetPipeline("v4l2src device=/dev/video0 ! videoconvert ! autovideosink");
-        stream.ListenOn("127.0.0.1", 8080);
+        stream.SetPipeline(pipeline);
+        stream.ListenOn(serverIp, serverPort);
 
         while (stream.IsListening()) 
         {
+            // Prevents throttling over the network and wasting CPU recources
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
         }
     }
