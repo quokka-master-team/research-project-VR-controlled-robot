a45812537ecff3e0b51294b9cdcda02ceedb33ab
Hubert Lewandowski <therootender@gmail.com>
2023-10-02 11:15:04 +0200

    Add program arguments

--- robot/camera/src/main.cpp
+++ robot/camera/src/main.cpp
@@ -1,12 +1,36 @@
-﻿#include "VideoStream.hpp"
+﻿#include <unistd.h>
+#include "VideoStream.hpp"
 
 int main(int argc, char *argv[])
 {
-    auto const streamName   = "Test stream";
-    auto const serverIp     = "127.0.0.1";
-    auto const serverPort   = 8080;
+    auto streamName   = "Test stream";
+    auto serverIp     = "127.0.0.1";
+    auto serverPort   = 8080;
 
     auto pipeline = "v4l2src device=/dev/video0 ! videoconvert ! autovideosink";
+
+    int opt;
+    while ((opt = getopt(argc, argv, "s:p:l:")) != -1)
+    {
+        switch (opt)
+        {
+            case 's':
+                serverIp = optarg;
+                break;
+            case 'p':
+                serverPort = std::stoi(optarg);
+                break;
+            case 'P':
+                pipeline = optarg;
+                break;
+            default:
+                printf("All possible options:\n");
+                printf("-P <gstreamer_pipeline>: Pipeline settings for camera configuration.\n");
+                printf("-s <server_ip>: IP of the server that requests commands.\n");
+                printf("-p <port>: Port of the server from which requests are sent.\n");
+                return 1;
+        }
+    }
 
     VideoStream stream(streamName);
 
