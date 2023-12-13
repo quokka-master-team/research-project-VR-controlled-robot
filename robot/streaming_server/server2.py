c661b609f1f2e3e83264218644e086d8c0a01008
Jakub Koniuszewski <67022758+JakubKoniuszewski@users.noreply.github.com>
2023-12-13 21:06:49 +0100

    Update server2.py

--- robot/streaming_server/server2.py
+++ robot/streaming_server/server2.py
@@ -2,12 +2,9 @@
 # Software License Agreement (BSD License)
 
 import socket
-import threading
-import multiprocessing
-from talker2 import RobotController
+from talker3 import RobotController
 
 class Server:
-    # Dictionary mapping commands to functions
     command_handlers = {
         'forward': 'forward_command',
         'left': 'left_command',
@@ -24,11 +21,8 @@ class Server:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.ip, self.port))
-        self.server_socket.listen(5)
-        self.client_handlers = {}
-        self.robot_controller = None
-        print(threading.current_thread())
-        print(threading.main_thread())
+        self.server_socket.listen(1)  # Allow only one connection
+        self.create_robot_controller()
 
     def start(self):
         print(f"Server listening on {self.ip}:{self.port}")
@@ -37,42 +31,39 @@ class Server:
             while True:
                 client_socket, client_address = self.server_socket.accept()
                 print(f"Connection request from {client_address}")
+                #print("Disarming Pixhawk...")
+                #self.robot_controller.disarm_pixhawk()
 
-                # Create a new thread to handle the client
-                client_handler = threading.Thread(target=self.handle_client, args=(client_socket, client_address))
-                client_handler.start()
-                self.create_robot_controller()
-                self.client_handlers[client_address] = client_handler
+                try:
+                    while True:
+                        data = client_socket.recv(1024).decode()
+                        if not data:
+                            break
+                        print(f"Received from {client_address}: {data}")
+                        self.process_command(data, client_address)
+
+                except KeyboardInterrupt:
+                        print("Server shutting down...")
+                        self.server_socket.close()
+                        print("Server shutdown complete.")
+
+                print(f"Connection with {client_address} closed.")
+                client_socket.close()
+                print("Disarming Pixhawk...")
+                self.robot_controller.disarm_pixhawk()
+
         except KeyboardInterrupt:
             print("Server shutting down...")
             self.server_socket.close()
-            for client_handler in self.client_handlers.values():
-                client_handler.join()
             print("Server shutdown complete.")
-
-    def handle_client(self, client_socket, client_address):
-        while True:
-            data = client_socket.recv(1024).decode()
-            if not data:
-                break
-            print(f"Received from {client_address}: {data}")
-            self.process_command(data, client_address)
-
-        print(f"Connection with {client_address} closed.")
-        del self.client_handlers[client_address]
-        client_socket.close()
-        print("Terminate ROS")
-        self.robot_controller.terminate_ros_process()
 
     def create_robot_controller(self):
         self.robot_controller = RobotController()
         self.robot_controller.init_node()
 
     def process_command(self, command, client_address):
-        # Use the dictionary to get the corresponding function
         command_method_name = self.command_handlers.get(command.lower())
         if command_method_name:
-            # Call the function with client_address as an argument
             command_method = getattr(self, command_method_name, None)
             if command_method:
                 command_method(client_address)
@@ -102,16 +93,13 @@ class Server:
         self.robot_controller.stop()
 
     def arm_command(self, client_address):
-        print(f"Robot armed - received command from {client_address}")
+        print(f"Received command ARM from {client_address}")
         self.robot_controller.arm_pixhawk()
 
     def disarm_command(self, client_address):
-        print(f"Robot disarmed - received command from {client_address}")
+        print(f"Received command DISARM from {client_address}")
         self.robot_controller.disarm_pixhawk()
 
 if __name__ == "__main__":
-    # Create an instance of the Server class with the desired IP and port
     server_instance = Server(ip='10.0.0.9', port=5001)
-
-    # Start the server
     server_instance.start()
